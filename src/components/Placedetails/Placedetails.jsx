8c4ed4627edb4a6b408a5a90bcbf761f0903e86c
SuryaTej-N <sxn2261@mavs.uta.edu>
2024-02-11 00:08:32 -0600

    just added text

---
+++ src/components/Placedetails/Placedetails.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const placedetails = () => {
+    return (
+        <div>
+            <h1>Placedetails</h1>
+        </div>
+    );
+}
+
+export default placedetails;
